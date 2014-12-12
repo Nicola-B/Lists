a0113547c9fa5b568adfe8a32007c731285f2380
40340 <40340@PC004139.coll.lr.local>
2014-12-12 14:15:41 +0000

    Lists Commit

---
+++ List starter Task 2.py
@@ -0,0 +1,15 @@
+#Nicola Batty
+#12/12/2014
+#List starter Task 1
+
+shopping_list = []
+finished = False
+while not finished:
+    shopping_item = input("Enter next item (-1 to end list): ")
+    if shopping_item == "-1":
+        finished = True
+    else:
+        shopping_list.append(shopping_item) #add new item to the list
+
+for index, shopping_item in enumerate(shopping_list):
+	print("Item {0} is {1}".format(index+1, shopping_item))
---
+++ Lists Starter Task 3.py
@@ -0,0 +1,13 @@
+#Nicola Batty
+#12/12/2014
+#Lists Starter Task 3
+
+values = [0,24,13,57,45]
+
+result = 0
+index = 0
+while index < 4:
+    index = index +1
+    if result < values[index]:
+        result = values[index]
+print (result)
