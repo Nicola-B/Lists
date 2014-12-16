c61b26ef2120435c4d596f53533d4076dca0e72c
40340 <40340@PC004139.coll.lr.local>
2014-12-16 13:02:52 +0000

    commit

---
+++ List Revision Exercise 1.py
@@ -0,0 +1,12 @@
+#Nicola Batty
+#16/12/2014
+#List Revision Exercise 1
+
+name_list = []#blank list
+
+for count in range(6):
+    name = input("please input a name: ")
+    name_list.append(name)
+
+for name in name_list:
+    print(name)
