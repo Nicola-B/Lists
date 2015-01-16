3c0e7d1a5cd26ff5c1f90fa8c0a78b33416c7701
Nicola <Nicola@172.16.204.228>
2015-01-16 10:20:17 +0000

    Commit

--- 2D List table function.py
+++ 2D List table function.py
@@ -2,6 +2,19 @@
 #06/01/2015
 #2D List table function
 
-def table(table):
-    for row in enumerate(table):
-        print("|{0}|{1}|{2}|".format(row[0], row[1], row[2]))
+def table(table1):
+    print("-"*27)
+    count = 0
+    for row in enumerate(table1):
+        print("|{0:<12}|{1:<5}|{2:<7}|".format(table1[count][0], table1[count][1], table1[count][2]))
+        print("-"*27)
+        count = count+1
+        
+#main program
+table1 = [
+    ["name", "kill", "deaths"],
+    ["k1llnAchine",51,49],
+    ["bob2247",5,99],
+    ["hAxOr12",72,30]
+    ]
+table(table1)
