b9c2be2d52f00a179e939d216f693e62145b4f4b
Nicola <Nicola@nicola-laptop2.lan>
2015-01-17 14:43:31 +0000

    Commit

--- Bubble sort function.py
+++ Bubble sort function.py
@@ -2,8 +2,6 @@
 #13/01/2015
 #Bubble sort function
 
-import pdb
-
 def input_list():
     number = int(input("How many in list: "))
     list1 = []
@@ -13,17 +11,18 @@ def input_list():
     return list1, number
 
 def sort_list(list1, number):
-    pdb.set_trace()
+    #pdb.set_trace()
     swaps = False
     while not swaps:
         swaps = True
         count = 0
         for itam in list1:
-            if itam > list1[count+1]:
-                swaps = False
-                temp = itam
-                list1[count] = list1[count+1]
-                list1[count+1] = temp
+            if count < number - 1:
+                if itam > list1[count+1]:
+                    swaps = False
+                    temp = itam
+                    list1[count] = list1[count+1]
+                    list1[count+1] = temp
                 count = count + 1
     return list1
 
@@ -32,8 +31,6 @@ def output_sorted_list(list1):
         print(itam)
 
 def sort():
-    #list1, number = input_list()
-    list1 = [10, 7, 3, 12, 1, 50, 90]
-    number = 7
+    list1, number = input_list()
     list1 = sort_list(list1, number)
     output_sorted_list(list1)
